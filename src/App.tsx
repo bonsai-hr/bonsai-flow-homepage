126d41818512ca1d04552db2d5074118f5b450f0
bonsai-hr <hatty@bonsai-hr.co.uk>
2025-10-29 10:49:43 +0000

    feat: add FloatingBackground to App

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import FloatingBackground from "./components/FloatingBackground";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <FloatingBackground />   {/* 👈 NEW LINE */}
       <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
