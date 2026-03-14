6c3ae5fe79a29bc1a213408b984d471736b4bffc
Rahat Choudhury <hatty@bonsai-hr.co.uk>
2026-03-14 08:56:27 +0000

    add 404 redirect for router

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
