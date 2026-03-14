dce14d51f35ac6e2a4172e26e54b2b58bf0dac2d
Rahat Choudhury <hatty@bonsai-hr.co.uk>
2026-03-14 09:00:22 +0000

    fix github pages route redirect

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const redirect = sessionStorage.redirect;
+const redirect = sessionStorage.getItem("redirect");
 if (redirect) {
-  delete sessionStorage.redirect;
+  sessionStorage.removeItem("redirect");
   window.history.replaceState(null, "", redirect);
 }
 
